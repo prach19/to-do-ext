1b3179563c4a3cd22dec35e2d3b441a0bc789f6f
prach19 <prachi19@my.yorku.ca>
2024-04-30 12:35:41 -0400

    using localStorage to keep track of tasks

--- todoli/todoli.js
+++ todoli/todoli.js
@@ -11,16 +11,29 @@ function addTask(){
         taskList.appendChild(li);
         let span = document.createElement("span");
         span.innerHTML = "\u00d7";
+        
     }
     inputTask.value="";
+    saveTasks();
 }
 
 taskList.addEventListener("click", function(input){
     if(input.target.tagName == "LI"){
         input.target.classList.toggle("checked");
+        saveTasks();
     }
     else if(input.target.tagName == "SPAN"){
         input.target.parentElement.remove();
+        saveTasks();
     }
 }, false);
 
+function saveTasks(){
+    localStorage.setItem("data", taskList.innerHTML);
+}
+
+function showTasks(){
+    taskList.innerHTML = localStorage.getItem("data");
+}
+
+showTasks();
